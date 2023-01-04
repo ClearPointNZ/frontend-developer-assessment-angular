8573c4da5e46e933b56be330ec8312e26d1736a9
Danny <danny.rietkerk@clearpoint.co.nz>
2023-01-04 13:28:16 +1300

    chore(husky): add pre-commit test and lint hooks

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -10,22 +10,10 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'TodoList'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('TodoList');
-  });
-
-  it('should render title', () => {
+  it('should display the title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('TodoList app is running!');
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.h4')?.textContent).toContain('Todo List App (Angular)');
   });
 });
